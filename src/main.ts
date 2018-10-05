c30b72ecacf65c91100a1210dca253b21eec666a
Philippe Leclerc <philippeleclerc299@gmail.com>
2018-10-04 20:21:08 -0400

    Basic logic to render images in page
    -> Follow up will be a tooltip-like popup

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,51 @@
 
-function test(): void {
-    console.log("test");
+/**
+ * Given an url, find the image for the manga.
+ *
+ * @param mangaPath URL to manga page
+ */
+function retrieveImagePathForManga(mangaPath: string): Promise<string> {
+    console.log(`Retrieving page ${mangaPath}`);
+
+    let imagePath: string;
+
+    return fetch(mangaPath)
+        .then((response) => response.text())
+        .then((text) => {
+            const parser = new DOMParser();
+            const htmlDocument = parser.parseFromString(text, "text/html");
+            const image: HTMLImageElement = htmlDocument.documentElement.querySelector("div.card-body > div > div > img");
+
+            imagePath = image.src;
+
+            console.log(imagePath);
+
+            return imagePath;
+        })
+        .catch((err) => {
+            console.log(err);
+            throw new Error("Unexpected error");
+        });
 }
 
-test();
+/**
+ * Add event listener on all the page's manga link.
+ */
+function addOnMouseOver() {
+
+    const selector = "div.chapter-container > div > div > a";
+
+    document.querySelectorAll(selector).forEach((element: HTMLLinkElement) => {
+        element.addEventListener("mouseover", () => {
+
+            const image: HTMLImageElement = document.createElement("img");
+
+            retrieveImagePathForManga(element.href).then((imagePath) => {
+                image.src = imagePath;
+                element.parentElement.appendChild(image);
+            });
+        });
+    });
+}
+
+addOnMouseOver();
