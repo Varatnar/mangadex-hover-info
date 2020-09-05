b769852056e9ed6f17aa10b66497de67efd76780
Philippe Leclerc <philippeleclerc299@gmail.com>
2020-09-05 00:33:01 -0400

    Now need local file to get tags

--- utilities/tagIndexRetriever.ts
+++ utilities/tagIndexRetriever.ts
@@ -1,49 +1,75 @@
 import * as fs from "fs";
 import { JSDOM } from "jsdom";
-import fetch from "node-fetch";
+import * as path from "path";
+import { pipe } from "ramda";
 import { Tag } from "../src/models/Tag";
 
 const tagsDataPath = "src/assets/tags.json";
 
+// todo: quick and dirty fix, change selector later
+const ignoredTagName = [
+    "Shounen",
+    "Shoujo",
+    "Seinen",
+    "Josei",
+    "Ongoing",
+    "Completed",
+    "Cancelled",
+    "Hiatus",
+];
+
+const orderById = (array: any[]) => {
+    return array.sort((a, b) => {
+        if (a.id > b.id) {
+            return 1;
+        } else if (a.id < b.id) {
+            return -1;
+        } else {
+            return 0;
+        }
+    });
+};
+
+const removeUnwantedTag = (array: any[]) => array.filter((tag) => !ignoredTagName.includes(tag.name));
+
+const cleanupTags = pipe(orderById, removeUnwantedTag);
+
 // todo: add more constants/variables (less hardcoding)
-(() => {
-    fetch("https://mangadex.org/search")
-        .then((response) => {
-            return response.text();
-        })
-        .then(async (html) => {
-            const tags: Tag[] = [];
-            const pageContent: HTMLElement = new JSDOM(html).window.document.documentElement;
+(async () => {
 
-            // Step one retrieve every checkbox sections
-            pageContent.querySelectorAll("div.custom-checkbox").forEach((element) => {
+    if (process.argv.length < 3) {
+        console.log("Need to pass a logged in search snapshot html file path");
+    }
 
-                let id: number;
-                let name: string;
+    const html = await fs.readFileSync(path.resolve(__dirname, process.argv[2]));
 
-                // get the id
-                id = parseInt(element.querySelector("input").getAttribute("value"), 10);
+    let tags: Tag[] = [];
 
-                // get the english name
-                name = element.querySelector("label > span").textContent;
+    const pageContent: HTMLElement = new JSDOM(html).window.document.documentElement;
 
-                tags.push(new Tag(id, name));
-            });
+    // Step one retrieve every checkbox sections
+    pageContent.querySelectorAll("div.custom-checkbox")
+               .forEach((element) => {
+                   let id: number;
+                   let name: string;
 
-            tags.sort((a, b) => {
-                if (a.id > b.id) {
-                    return 1;
-                } else if (a.id < b.id) {
-                    return -1;
-                } else {
-                    return 0;
-                }
-            });
+                   // get the id
+                   id = parseInt(element.querySelector("input").getAttribute("value"), 10);
 
-            try {
-                await fs.writeFileSync(tagsDataPath, JSON.stringify(tags, null, 2), "utf-8");
-            } catch (error) {
-                console.error(error);
-            }
-        });
+                   // get the english name
+                   name = element.querySelector("label").textContent;
+
+                   tags.push(new Tag(id, name));
+               });
+
+    tags = cleanupTags(tags);
+
+    try {
+        console.log(`${tags.length} tags will be saved...`);
+        fs.writeFileSync(tagsDataPath, JSON.stringify(tags, null, 2), "utf-8");
+        console.log(`Tags saved to ${tagsDataPath}`);
+    } catch (error) {
+        console.error(error);
+    }
+
 })();
